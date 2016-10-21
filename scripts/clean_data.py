5009a07e48c37e81bce5f958480513a51e362247
Tim Gasser <timgasser@hotmail.com>
2016-10-20 21:57:57 -0500

    Finished off bike and station reading including station id

--- scripts/clean_data.py
+++ scripts/clean_data.py
@@ -10,7 +10,7 @@ import pandas as pd
 from tqdm import tqdm
 
 HTML_DIR = '../data/html'
-DATA_DIR = '../data'
+DATA_DIR = '../input'
 
 LAT_IDX = 0
 LONG_IDX = 1
@@ -18,7 +18,7 @@ LONG_IDX = 1
 STAT_NAME = 0
 STAT_ADDRESS = 1
 STAT_BIKES = 2
-STAT_AVAIL = 3
+STAT_DOCKS = 3
 
 files = glob(HTML_DIR + '/*.html')
 # num_files = len(files)
@@ -35,14 +35,14 @@ latlong_re = re.compile('var point = new google\.maps\.LatLng\((.+), (.+)\);')
 
 # Dictionary to store stations (fast lookup by lat/lon)
 stations = dict()
-
+station_id = 1
 # List to store the bike values for each station in a series
 bike_list = list()
 
 
 file_counter = 0
-verbose = False
-short_run = False
+verbose = True
+short_run = True
 
 for bike_idx, bike_filename in tqdm(enumerate(files),  total=len(files)):
     file_counter += 1
@@ -73,10 +73,11 @@ for bike_idx, bike_filename in tqdm(enumerate(files),  total=len(files)):
                 address = station_re.match(line).groups()[STAT_ADDRESS].replace('<br />', ', ')
                 address.replace('<br />', ', ')
                 bikes = int(station_re.match(line).groups()[STAT_BIKES])
-                avail = int(station_re.match(line).groups()[STAT_AVAIL])
+                docks = int(station_re.match(line).groups()[STAT_DOCKS])
 
                 if (latlon not in stations):
                     new_station = dict()
+                    new_station['station_id'] = station_id
                     new_station['name'] = name
                     new_station['address'] = address
                     new_station['lat'] = latitude
@@ -85,28 +86,38 @@ for bike_idx, bike_filename in tqdm(enumerate(files),  total=len(files)):
                     new_station['time'] = time
                     stations[latlon] = new_station
 
+                    station_id += 1
                     if verbose:
                         print('Adding new station: {}'.format(new_station))
 
                 new_bike = dict()
+                new_bike['station_id'] = stations[latlon]['station_id']
                 new_bike['latlon'] = latlon
-                new_bike['datetime'] = datetime_string
+                new_bike['date'] = date
+                new_bike['time'] = time
                 new_bike['bikes'] = bikes
-                new_bike['avail'] = avail
+                new_bike['docks'] = docks
                 bike_list.append(new_bike)
                 if verbose:
-                    print('Lat = {}, lon = {}, bikes = {}, avail = {}'.format(latitude, longitude, bikes, avail))
+                    print('Lat = {}, lon = {}, bikes = {}, docks = {}'.format(latitude, longitude, bikes, docks))
                 # print 'Found {} stations'.format(len(station_list))
 
 
 print('Found {} stations'.format(len(stations)))
-print('Found records from {} to {}'.format(bike_list[0]['datetime'], bike_list[-1]['datetime']))
+print('Found records from {} to {}'.format(bike_list[0]['date'], bike_list[-1]['date']))
 
 # print('Converting station and bike data to csv file')
 #
 # # Convert to pandas dataframes
 # # stations_inv_dict = {v: k for k, v in stations_dict.items()}
-# # stations_df = pd.DataFrame(station_list)
+stations_df = pd.DataFrame.from_dict(stations, orient='index')
+stations_df.to_csv(DATA_DIR + '/stations.csv')
+
+bikes_df = pd.DataFrame(bike_list)
+bikes_df = bikes_df[['station_id', 'date', 'time', 'bikes', 'docks']]
+print(bikes_df)
+bikes_df.to_csv(DATA_DIR + '/bikes.csv', index=False)
+
 # # stations_df = stations_df[['station_id', 'name', 'address', 'lat', 'long', 'capacity']]
 #
 # bikes_df = pd.DataFrame(bike_list)
@@ -117,5 +128,4 @@ print('Found records from {} to {}'.format(bike_list[0]['datetime'], bike_list[-
 # print('Bikes dataframe')
 # print(bikes_df)
 #
-# stations_df.to_csv(DATA_DIR + '/stations.csv', index=False)
 # bikes_df.to_csv(DATA_DIR + '/bikes.csv', index=False)
