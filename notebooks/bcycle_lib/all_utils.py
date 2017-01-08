12338fa82320ab9ae53d5520cb8f49c8b60befc6
Tim Gasser <timgasser@hotmail.com>
2017-01-07 23:46:59 -0600

    Added all weather CSV file, basic linear regression model.

--- notebooks/bcycle_lib/all_utils.py
+++ notebooks/bcycle_lib/all_utils.py
@@ -4,6 +4,11 @@ import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
 
+# sklearn section
+from sklearn.preprocessing import LabelBinarizer, MinMaxScaler, scale
+
+
+
 INPUT_DIR = '../input'
 
 # All-data utilities
@@ -81,6 +86,61 @@ def load_bcycle_data(directory, station_filename, trips_filename, verbose=False)
 
     return (stations_df, trips_df)
 
+
+def clean_weather(df):
+    '''Cleans weather dataframe'''
+
+    # Remove whitespace and keep min/max values
+    df.columns = [col.strip() for col in df.columns]
+    date_col = df.columns[0]
+    df = df[[date_col,'Max TemperatureF','Min TemperatureF', 
+             'Max Humidity', 'Min Humidity',
+             'Max Sea Level PressureIn', 'Min Sea Level PressureIn', 
+             'Max Wind SpeedMPH', 'Mean Wind SpeedMPH', 'Max Gust SpeedMPH',
+             'PrecipitationIn', 'CloudCover', 'Events']]
+    
+    # Clean up column names, drop means as they're a linear combination of max/min
+    df.columns = ['date', 'max_temp', 'min_temp', 'max_humidity', 'min_humidity',
+                 'max_pressure', 'min_pressure', 'max_wind', 'min_wind', 'max_gust',
+                 'precipitation', 'cloud_cover', 'events']
+    
+    # Convert column types appropriately
+    df['date'] = pd.to_datetime(df['date'], format='%Y-%m-%d')  
+    df = df.set_index('date', drop=True)
+    
+    df[['max_temp', 'min_temp']] = df[['max_temp', 'min_temp']].astype(np.uint8)
+    df[['max_humidity', 'min_humidity']] = df[['max_humidity', 'min_humidity']].astype(np.uint8)
+    df[['max_wind', 'min_wind', 'max_gust']] = df[['max_wind', 'min_wind', 'max_gust']].astype(np.uint8)
+
+
+    # Cloud cover is a fraction of 8 - 
+    # http://help.wunderground.com/knowledgebase/articles/129043-how-can-i-translate-the-cloud-cover-data-on-your
+    df['cloud_pct'] = (df['cloud_cover'].astype(np.float32) / 8.0) * 100
+    df = df.drop('cloud_cover', axis=1)
+    
+    # Precipitation sometimes has 'T' for trace amounts of rain. Replace this with small value
+    # and convert to a float
+    # http://help.wunderground.com/knowledgebase/articles/656875-what-does-t-stand-for-on-the-rain-precipitation
+    df['precipitation'] = df['precipitation'].replace('T', 0.01)
+    df['precipitation'] = df['precipitation'].astype(np.float32)
+
+    # Events are tricky. they're separated by hypens, and can have multiple values not in the same order !
+    events = set()
+    df['events'] = df['events'].replace(np.nan, 'None')
+    for row in df['events']:
+        if row is not np.nan:
+            line = row.split('-')
+            [events.add(word.lower()) for word in line]
+    
+    for event in events:
+        df[event] = df['events'].apply(str.lower).str.contains(event).astype(np.uint8)
+    
+    df = df.drop(['events', 'none'], axis=1)
+    return df
+
+
+
+
 # Plotting functions
 
 def plot_lines(df, subplots, title, xlabel, ylabel):
@@ -140,4 +200,208 @@ def plot_hist(df_col, bins, size, title, xlabel, ylabel):
     ax.tick_params(axis='y', labelsize=14)   
 
 
+# Model evaluation plotting functions
 
+def df_from_results(index_train, y_train, y_train_pred, index_val, y_val, y_val_pred):
+    '''Creates dataframe from results for use in plotting functions below'''
+    train_dict = dict()
+    val_dict = dict()
+
+    train_dict['true'] = y_train
+    train_dict['pred'] = y_train_pred
+
+    val_dict['true'] = y_val
+    val_dict['pred'] = y_val_pred
+
+    train_df = pd.DataFrame(train_dict)
+    val_df = pd.DataFrame(val_dict)
+
+    train_df.index = index_train
+    val_df.index = index_val
+    
+    return train_df, val_df
+    
+   
+
+def plot_val(val_df, pred_col, true_col, title):
+    '''
+    Plots the validation prediction
+    INPUT: val_df - Validation dataframe
+           pred_col - string with prediction column name
+           true_col - string with actual column name
+           title - Prefix for the plot titles.
+    RETURNS: Nothing
+    '''
+    def plot_ts(df, pred, true, title, ax):
+        '''Generates one of the subplots to show time series'''
+        ax = df.plot(y=[pred, true], ax=ax) # , color='black', style=['--', '-'])
+        ax.set_xlabel('Date', fontdict={'size' : 14})
+        ax.set_ylabel('Rentals', fontdict={'size' : 14})
+        ax.set_title(title, fontdict={'size' : 16}) 
+        ttl = ax.title
+        ttl.set_position([.5, 1.02])
+        ax.legend(['Predicted rentals', 'Actual rentals'], fontsize=14, loc=2)
+        ax.tick_params(axis='x', labelsize=14)
+        ax.tick_params(axis='y', labelsize=14)
+    
+    fig, ax = plt.subplots(1,1, sharey=True, figsize=(16,8))
+    plot_ts(val_df, pred_col, true_col, title + ' (validation set)', ax)
+    
+
+def plot_prediction(train_df, val_df, pred_col, true_col, title):
+    '''
+    Plots the predicted rentals along with actual rentals for the dataframe
+    INPUT: train_df, val_df - pandas dataframe with training and validataion results
+           pred_col - string with prediction column name
+           true_col - string with actual column name
+           title - Prefix for the plot titles.
+    RETURNS: Nothing
+    '''
+    def plot_ts(df, pred, true, title, ax):
+        '''Generates one of the subplots to show time series'''
+        ax = df.plot(y=[pred, true], ax=ax) # , color='black', style=['--', '-'])
+        ax.set_xlabel('', fontdict={'size' : 14})
+        ax.set_ylabel('Rentals', fontdict={'size' : 14})
+        ax.set_title(title, fontdict={'size' : 16}) 
+        ttl = ax.title
+        ttl.set_position([.5, 1.02])
+        ax.legend(['Predicted rentals', 'Actual rentals'], fontsize=14)
+        ax.tick_params(axis='x', labelsize=14)
+        ax.tick_params(axis='y', labelsize=14)   
+    
+    fig, axes = plt.subplots(2,1, sharey=True, figsize=(20,12))
+    plot_ts(train_df, pred_col, true_col, title + ' (training set)', axes[0])
+    plot_ts(val_df, pred_col, true_col, title + ' (validation set)', axes[1])
+    
+def plot_residuals(train_df, val_df, pred_col, true_col, title):
+    '''
+    Plots the residual errors in histogram (between actual and prediction)
+    INPUT: train_df, val_df - pandas dataframe with training and validataion results
+           pred_col - string with prediction column name
+           true_col - string with actual column name
+           title - Prefix for the plot titles.
+    RETURNS: Nothing
+
+    '''
+    def plot_res(df, pred, true, title, ax):
+        '''Generates one of the subplots to show time series'''
+        residuals = df[pred] - df[true]
+        ax = residuals.plot.hist(ax=ax, bins=20)
+        ax.set_xlabel('Residual errors', fontdict={'size' : 14})
+        ax.set_ylabel('Count', fontdict={'size' : 14})
+        ax.set_title(title, fontdict={'size' : 16}) 
+        ttl = ax.title
+        ttl.set_position([.5, 1.02])
+        ax.tick_params(axis='x', labelsize=14)
+        ax.tick_params(axis='y', labelsize=14)   
+    
+    fig, axes = plt.subplots(1,2, sharey=True, sharex=True, figsize=(20,6))
+    plot_res(train_df, pred_col, true_col, title + ' residuals (training set)', axes[0])
+    plot_res(val_df, pred_col, true_col, title + ' residuals (validation set)', axes[1])
+    
+    
+def plot_results(train_df, val_df, pred_col, true_col, title):
+    '''Plots time-series predictions and residuals'''
+    plot_prediction(train_df, val_df, pred_col, true_col, title=title)
+    plot_residuals(train_df, val_df, pred_col, true_col, title=title)
+    
+def plot_scores(df, title, sort_col=None):
+    '''Plots model scores in a horizontal bar chart
+    INPUT: df - dataframe containing train_rmse and val_rmse columns
+           sort_col - Column to sort bars on
+    RETURNS: Nothing
+    '''
+    fig, ax = plt.subplots(1,1, figsize=(12,8)) 
+    if sort_col is not None:
+        scores_df.sort_values(sort_col).plot.barh(ax=ax)
+    else:
+        scores_df.sort_values(sort_col).plot.barh(ax=ax)
+
+    ax.set_xlabel('RMSE', fontdict={'size' : 14})
+    ax.set_title(title, fontdict={'size' : 18}) 
+    ttl = ax.title
+    ttl.set_position([.5, 1.02])
+    ax.legend(['Train RMSE', 'Validation RMSE'], fontsize=14, loc=0)
+    ax.tick_params(axis='x', labelsize=14)
+    ax.tick_params(axis='y', labelsize=14)
+
+
+    
+# Model training functions
+
+def reg_x_y_split(df, target_col, ohe_cols=None, z_norm_cols=None, minmax_norm_cols=None):
+    ''' Returns X and y to train regressor
+    INPUT: df = Dataframe to be converted to numpy arrays 
+           target_col = Column name of the target variable
+           ohe_col = Categorical columns to be converted to one-hot-encoding
+           z_norm_col = Columns to be z-normalized
+    RETURNS: Tuple with X, y, df
+    '''
+    
+    # Create a copy, remove index and date fields
+    df_out = df.copy()
+    df_X = df.copy()
+    df_X = df_X.reset_index(drop=True)
+    X = None
+    
+    # Convert categorical columns to one-hot encoding
+    if ohe_cols is not None:
+        for col in ohe_cols:
+            print('Binarizing column {}'.format(col))
+            lbe = LabelBinarizer()
+            ohe_out = lbe.fit_transform(df_X[col])
+            if X is None:
+                X = ohe_out
+            else:
+                X = np.hstack((X, ohe_out))
+            df_X = df_X.drop(col, axis=1)
+            
+    # Z-normalize relevant columns
+    if z_norm_cols is not None:
+        for col in z_norm_cols:
+            print('Z-Normalizing column {}'.format(col))
+            scaled_col = scale(df[col].astype(np.float64))
+            scaled_col = scaled_col[:,np.newaxis]
+            df_out[col] = scaled_col
+            if X is None:
+                X = scaled_col
+            if X is not None:
+                X = np.hstack((X, scaled_col))
+            df_X = df_X.drop(col, axis=1)
+
+    if minmax_norm_cols is not None:
+        for col in minmax_norm_cols:
+            print('Min-max scaling column {}'.format(col))
+            mms = MinMaxScaler()
+            mms_col = mms.fit_transform(df_X[col])
+            mms_col = mms_col[:, np.newaxis]
+            df_out[col] = mms_col
+            if X is None:
+                X = mms_col
+            else:
+                X = np.hstack((X, mms_col))
+            df_X = df_X.drop(col, axis=1)
+
+    # Combine raw pandas Dataframe with encoded / normalized np arrays
+    if X is not None:
+        X = np.hstack((X, df_X.drop(target_col, axis=1).values))
+    else:
+        X = df_X.drop(target_col, axis=1)
+        
+    y = df[target_col].values
+
+    return X, y, df_out
+
+
+def add_time_features(df):
+    ''' Extracts dayofweek and hour fields from index
+    INPUT: Dataframe to extract fields from
+    RETURNS: Dataframe with dayofweek and hour columns
+    
+    '''
+    df.loc[:,'dayofweek'] = df.index.dayofweek
+    df.loc[:,'hour'] = df.index.hour.astype(str)
+    df.loc[:,'day-hour'] = df['dayofweek'].astype(str) + '-' + df['hour']
+    df.loc[:,'weekday'] = (df['dayofweek'] < 5).astype(np.uint8)
+    df.loc[:,'weekend'] = (df['dayofweek'] >= 5).astype(np.uint8)
+    return df
